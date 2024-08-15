b863bf4076168fd1edf6c401bc7c161bbfba7df2
nico <ngalfond@ercbroadband.com>
2024-08-15 14:15:00 -0400

    trying to break merge

--- cron_experiment.py
+++ cron_experiment.py
@@ -2,7 +2,7 @@ import datetime
 
 x = datetime.datetime.now()
 
-print(f"this was edited on the MAIN branch, the time is {x}")
+print(f"this was edited on the OTHER branch, the time is {x}")
 
 print("but this was added in the branch")
 
